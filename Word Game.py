69f8d38f913263f7eb58a39b810ba66fd0b78c30
TheAdmiralMoses <125628727+TheAdmiralMoses@users.noreply.github.com>
2023-02-18 15:00:34 -0500

    Update Word Game.py

--- Word Game.py
+++ Word Game.py
@@ -1,1 +1,14 @@
-Print ("Welcome to my word guessing game, type a word and if you match a letter where it goes it will appear in uppercase, if not it will appear in lowercase.")
+import time
+import math
+print ("Welcome to my word guessing game, type a word and if you match a letter where it goes it will appear in uppercase, if not it will appear in lowercase.")
+time.sleep(3)
+TestWord = ""
+Guesses = int(0)
+while TestWord != "Macaroni":
+    TestWord = input("Input a word to guess! ")
+    Guesses = Guesses + 1
+print ("Congrats, you win!\n")
+if Guesses <2:
+    print (f"You made {Guesses} guess.")
+else:
+    print (f"You made {Guesses} guesses.")
