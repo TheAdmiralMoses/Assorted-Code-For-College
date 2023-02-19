fdeb719f22910c7c1897a05ac156db1ef67dba66
TheAdmiralMoses <125628727+TheAdmiralMoses@users.noreply.github.com>
2023-02-19 16:23:45 -0500

    Update Word Game.py

--- Word Game.py
+++ Word Game.py
@@ -2,7 +2,16 @@ import time
 import math
 print ("Welcome to my word guessing game, type a word and if you match a letter where it goes it will appear in uppercase, if not it will appear in lowercase.")
 time.sleep(3)
-KeyWord = Macaroni
+KeyLetter1 = "m"
+KeyLetter2 = "a"
+KeyLetter3 = "c"
+KeyLetter4 = "a"
+KeyLetter5 = "r"
+KeyLetter6 = "o"
+KeyLetter7 = "n"
+KeyLetter8 = "i"
+KeyWord = KeyLetter1 + KeyLetter2 + KeyLetter3 + KeyLetter4 + KeyLetter5 + KeyLetter6 + KeyLetter7 + KeyLetter8
+
 TestWord = ""
 Hint = "_ _ _ _ _ _ _ _"
 Guesses = int(0)
