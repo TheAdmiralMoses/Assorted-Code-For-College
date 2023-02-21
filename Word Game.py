626b0eb2ca58e2a5ffdb0c7d33fab0ab2d09641c
TheAdmiralMoses <125628727+TheAdmiralMoses@users.noreply.github.com>
2023-02-21 17:30:05 -0500

    Update Word Game.py
    Yeet

--- Word Game.py
+++ Word Game.py
@@ -1,23 +1,49 @@
 import time
 import math
-print ("Welcome to my word guessing game, type a word and if you match a letter where it goes it will appear in uppercase, if not it will appear in lowercase.")
-time.sleep(3)
-KeyWord = Macaroni
-TestWord = ""
-Hint = "_ _ _ _ _ _ _ _"
-Guesses = int(0)
-while TestWord != KeyWord:
-    print (f"Hint: {Hint}")
-    TestWord = input("Input a word to guess! ")
-    list(TestWord)
-    for letter in TestWord:
-        if letter == KeyWord:
+import random
+def start_game():
+    KeyWordList = ["moroni","alma","nephi"]
+    print ("Welcome to my word guessing game, type a word and if you match a letter where it goes it will appear in uppercase, if not it will appear in lowercase.")
+    TestWord = ""
+    KeyWord = random.choice(KeyWordList)
+    Guesses = int(0)
+    Hint = ""
+    for letter in KeyWord:
+        Hint = Hint + " _"
+    while TestWord != KeyWord:
+        print (f"Your hint is:{Hint}")
+        TestWord = input("Input a word to guess! ")
+        TestWord = TestWord.lower()
+        if len(TestWord) < len(KeyWord):
+            print ("That word is too short")
+        elif len(TestWord) > len(KeyWord):
+            print ("That word is too long")
+        else:
+            Hint = ""
+            for i, letter in enumerate(TestWord):
+                for i2, letter2 in enumerate(KeyWord):
+                    if i == i2 and letter ==  letter2:
+                        letter = letter.upper()
+                        Hint += f" {letter}"
+                        break
+                else:
+                    if letter in KeyWord:
+                        Hint += f" {letter}"
+                    else:
+                        Hint = Hint + " _"
+        Guesses = Guesses + 1
+    print ("Congrats, you win!\n")
+    if Guesses <2:
+        print (f"You made {Guesses} guess.")
+    else:
+        print (f"You made {Guesses} guesses.")
+    while True:
+        replay = input("Play again? Type Y or N")
+        if replay == "Y":
+            start_game()
+        elif replay == "N":
             break
         else:
-            break
-    Guesses = Guesses + 1
-print ("Congrats, you win!\n")
-if Guesses <2:
-    print (f"You made {Guesses} guess.")
-else:
-    print (f"You made {Guesses} guesses.")
+            print("That is not an option, try again")
+if __name__ == "__main__":
+    start_game()
